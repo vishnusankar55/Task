5ba4742c63e8833f3f67cdd0b0cb252d9ba119ad
vishnusankar55 <vishnusankar55@gmail.com>
2023-12-18 12:44:19 +0530

    Intial commit

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ DATABASE_URL_MONGO = "mongodb://localhost:27017"
 
 Base = declarative_base()
 
-# Define PostgreSQL model
 class UserPostgres(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
@@ -19,22 +18,18 @@ class UserPostgres(Base):
     password = Column(String)
     phone = Column(String)
 
-# Create PostgreSQL engine and session
 engine_postgres = create_engine(DATABASE_URL_POSTGRES)
 SessionPostgres = sessionmaker(autocommit=False, autoflush=False, bind=engine_postgres)
 
-# Define MongoDB model
 class UserProfileMongo(BaseModel):
     full_name: str
     profile_picture: str
 
-# Create MongoDB client
 client_mongo = AsyncIOMotorClient(DATABASE_URL_MONGO)
 db_mongo = client_mongo.get_database()
 
 app = FastAPI()
 
-# Dependency to get PostgreSQL database session
 def get_db_postgres():
     db = SessionPostgres()
     try:
@@ -42,14 +37,12 @@ def get_db_postgres():
     finally:
         db.close()
 
-# Dependency to get MongoDB database
 async def get_db_mongo():
     try:
         yield db_mongo
     finally:
         client_mongo.close()
 
-# Create user in both PostgreSQL and MongoDB
 @app.post("/register")
 async def register(
     full_name: str = Form(...),
@@ -60,17 +53,14 @@ async def register(
     db_postgres: SessionPostgres = Depends(get_db_postgres),
     db_mongo: AsyncIOMotorClient = Depends(get_db_mongo)
 ):
-    # Check if email already exists
     if db_postgres.query(UserPostgres).filter(UserPostgres.email == email).first():
         raise HTTPException(status_code=400, detail="Email already registered")
 
-    # Save user details to PostgreSQL
     user_postgres = UserPostgres(full_name=full_name, email=email, password=password, phone=phone)
     db_postgres.add(user_postgres)
     db_postgres.commit()
     db_postgres.refresh(user_postgres)
 
-    # Save profile picture to MongoDB
     if profile_picture:
         profile_picture_content = await profile_picture.read()
         profile_picture_str = profile_picture_content.decode("utf-8")
@@ -78,3 +68,27 @@ async def register(
         await db_mongo.users.insert_one(user_mongo.dict())
 
     return {"message": "User registered successfully"}
+
+
+@app.get("/user/{user_id}")
+async def get_user_details(
+    user_id: int,
+    db_postgres: SessionPostgres = Depends(get_db_postgres),
+    db_mongo: AsyncIOMotorClient = Depends(get_db_mongo)
+):
+    user_postgres = db_postgres.query(UserPostgres).filter(UserPostgres.id == user_id).first()
+    if not user_postgres:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    user_mongo = await db_mongo.users.find_one({"full_name": user_postgres.full_name})
+    if not user_mongo:
+        raise HTTPException(status_code=404, detail="User not found in MongoDB")
+
+    return {
+        "user_id": user_postgres.id,
+        "full_name": user_postgres.full_name,
+        "email": user_postgres.email,
+        "phone": user_postgres.phone,
+        "profile_picture": user_mongo.get("profile_picture", None)
+    }
+
