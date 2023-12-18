114633737c04242ba4fe3b574c0e27af406c2d20
vishnusankar55 <vishnusankar55@gmail.com>
2023-12-18 12:36:26 +0530

    Intial commit

---
+++ app.py
@@ -0,0 +1,80 @@
+from fastapi import FastAPI, HTTPException, Depends, File, UploadFile, Form
+from sqlalchemy import create_engine, Column, Integer, String, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from motor.motor_asyncio import AsyncIOMotorClient
+from pydantic import BaseModel
+
+DATABASE_URL_POSTGRES = "postgresql://user:password@localhost/dbname"
+DATABASE_URL_MONGO = "mongodb://localhost:27017"
+
+Base = declarative_base()
+
+# Define PostgreSQL model
+class UserPostgres(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    full_name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+    phone = Column(String)
+
+# Create PostgreSQL engine and session
+engine_postgres = create_engine(DATABASE_URL_POSTGRES)
+SessionPostgres = sessionmaker(autocommit=False, autoflush=False, bind=engine_postgres)
+
+# Define MongoDB model
+class UserProfileMongo(BaseModel):
+    full_name: str
+    profile_picture: str
+
+# Create MongoDB client
+client_mongo = AsyncIOMotorClient(DATABASE_URL_MONGO)
+db_mongo = client_mongo.get_database()
+
+app = FastAPI()
+
+# Dependency to get PostgreSQL database session
+def get_db_postgres():
+    db = SessionPostgres()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# Dependency to get MongoDB database
+async def get_db_mongo():
+    try:
+        yield db_mongo
+    finally:
+        client_mongo.close()
+
+# Create user in both PostgreSQL and MongoDB
+@app.post("/register")
+async def register(
+    full_name: str = Form(...),
+    email: str = Form(...),
+    password: str = Form(...),
+    phone: str = Form(...),
+    profile_picture: UploadFile = File(None),
+    db_postgres: SessionPostgres = Depends(get_db_postgres),
+    db_mongo: AsyncIOMotorClient = Depends(get_db_mongo)
+):
+    # Check if email already exists
+    if db_postgres.query(UserPostgres).filter(UserPostgres.email == email).first():
+        raise HTTPException(status_code=400, detail="Email already registered")
+
+    # Save user details to PostgreSQL
+    user_postgres = UserPostgres(full_name=full_name, email=email, password=password, phone=phone)
+    db_postgres.add(user_postgres)
+    db_postgres.commit()
+    db_postgres.refresh(user_postgres)
+
+    # Save profile picture to MongoDB
+    if profile_picture:
+        profile_picture_content = await profile_picture.read()
+        profile_picture_str = profile_picture_content.decode("utf-8")
+        user_mongo = UserProfileMongo(full_name=full_name, profile_picture=profile_picture_str)
+        await db_mongo.users.insert_one(user_mongo.dict())
+
+    return {"message": "User registered successfully"}
